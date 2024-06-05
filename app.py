6724c5d1c508b0383ce989f76bb7f49ff5cb2e33
Brock Jenkinson <brockjenkinson@gmail.com>
2024-06-05 16:33:11 -0400

    fixed task list variable name and add task removal functionality

--- app.py
+++ app.py
@@ -3,16 +3,24 @@
 #-------------------------
 
 #here is a global list I will use to store the tasks
-tasks_list = []
+task_list = []
 
 #defining the variables and functions
-
+# add task
 def add_task(task, date):
-    tasks_list.append({'task': task, 'date': date, 'status': 'incomplete'})
+    task_list.append({'task': task, 'date': date, 'status': 'incomplete'})
     print("The task,", task, ",added successfully!")
+# remove task
+def remove_task(index):
+    #check if the index is valid
+    if 0 <= index < len(task_list):
+         removed_task = task_list.pop(index)
+         print("Removed task:", removed_task['task'])
+    else:
+        print("Invalid index!")
+# mark task as completed
 
-
-#welcoming message to user
+#main loop starts here
 print("Welcome to the To-Do List App!")
 print("Please choose an option:")
 print("1. Add a task\n2. Remove a task\n3. Mark a task as completed\n4. View tasks\n5. Exit")
@@ -21,5 +29,5 @@ print("1. Add a task\n2. Remove a task\n3. Mark a task as completed\n4. View tas
 option = int(input("Enter your option: "))
 if option == 1:
         task = input("Enter the task: ")
-        date = input("Enter the date: ")
+        date = input("Enter the date (XX/XX/XXXX): ")
         add_task(task, date)
